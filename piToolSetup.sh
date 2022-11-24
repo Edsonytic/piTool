9bd11547e9872c23f4a074d2802c016abe23f419
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-11-24 09:14:01 -0600

    add old piTool removal
    Remove the previous version to make way for updates

--- piToolSetup.sh
+++ piToolSetup.sh
@@ -2,4 +2,5 @@
 
 wget -O piTool.zip https://github.com/Edsonytic/piTool/archive/refs/heads/main.zip  && unzip piTool.zip && rm piTool.zip
 chmod -R +x piTool-main
+sudo rm -R /usr/bin/piTool
 sudo mv piTool-main /usr/bin/piTool
