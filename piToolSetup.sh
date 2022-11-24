df122a6585adcea91697e363d2281d6f96b3ab17
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-11-24 09:04:42 -0600

    correct directory name
    add -main for unziped folder

--- piToolSetup.sh
+++ piToolSetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget -O piTool.zip https://github.com/Edsonytic/piTool/archive/refs/heads/main.zip  && unzip piTool.zip && rm piTool.zip
-chmod -R +x piTool
-sudo mv piTool /usr/bin/piTool
+chmod -R +x piTool-main
+sudo mv piTool-main /usr/bin/piTool
