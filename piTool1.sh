67c7e94fc573ff820e806ecc02dca696fe4205f6
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-11-27 08:50:39 -0600

    correct sub-script paths
    Use the script's directory as a variable

--- piTool1.sh
+++ piTool1.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-source /setup/netcfg.sh
-source /setup/apt-setup.sh
+setenv SCRIPTDIR `dirname $0`
+
+source $SCRIPTDIR/setup/netcfg.sh
+source $SCRIPTDIR/setup/apt-setup.sh
 sudo raspi-config
 sudo reboot
