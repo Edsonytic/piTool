54ba8785a8df7312eaa74a5d6395b346e9f8c2e3
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-11-27 19:56:54 -0600

    add chechking for updates

--- setup/apt-setup.sh
+++ setup/apt-setup.sh
@@ -2,5 +2,5 @@
 
 sudo bash -c "echo '# Added by piTool
 deb http://deb.debian.org/debian bullseye-backports main contrib non-free'>> /etc/apt/sources.list"
-sudo apt-get dist-upgrade -y
-sudo apt-get install unattended-upgrades
+sudo apt-get update && sudo apt-get dist-upgrade -y
+sudo apt-get install unattended-upgrades -y
