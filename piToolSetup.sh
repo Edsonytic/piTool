a8a8290e27e5d4d80b3303d4b9cc43aa65e033b0
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-12-07 00:09:52 -0600

    correct installation according to FHS

--- piToolSetup.sh
+++ piToolSetup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-wget -O piTool.zip https://github.com/Edsonytic/piTool/archive/refs/heads/main.zip  && unzip piTool.zip && rm piTool.zip
-chmod -R +x piTool-main
-sudo rm -R /usr/local/bin/piTool
-sudo mv piTool-main /usr/local/bin/piTool
+wget -O piTool.zip https://github.com/Edsonytic/piTool/archive/refs/heads/main.zip && unzip piTool.zip && rm piTool.zip
+chmod -R +x piTool-main && cd piTool-main
+sudo mv piTool1.sh piTool2.sh /usr/local/bin/
