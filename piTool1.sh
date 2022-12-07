6ee10b72d46f7a83ccfc6354f9c19ff57475caa3
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-12-07 00:08:32 -0600

    integrate individual scripts

--- piTool1.sh
+++ piTool1.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-source /usr/local/bin/piTool/setup/netcfg.sh
-source /usr/local/bin/piTool/setup/apt-setup.sh
+sudo nano /etc/dhcpcd.conf && sudo nano /etc/hosts && sudo service dhcpcd restart
+sudo bash -c "echo '# Added by piTool
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free'>> /etc/apt/sources.list" && sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get install unattended-upgrades -y
 sudo raspi-config
 sudo reboot
--- setup/apt-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo bash -c "echo '# Added by piTool
-deb http://deb.debian.org/debian bullseye-backports main contrib non-free'>> /etc/apt/sources.list"
-sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get install unattended-upgrades -y
