025ce21ef985fd92fb5a66b0691699423e5595ed
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2022-12-24 09:17:15 -0600

    correct creation of folddrs

--- install/certbot.sh
+++ install/certbot.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install -y certbot python3-certbot-dns-cloudflare python3-certbot-apache
 read -p 'Please provide an API Token for DNS on Cloudflare: ' CF_TOKEN
-try mkdir ~/.secrets && try mkdir ~/.secrets/certbot && echo '# Cloudflare API token used by Certbot
-dns_cloudflare_api_token = $CF_TOKEN'>> ~/.secrets/certbot/cloudflare.ini && chmod 600 ~/.secrets/certbot/cloudflare.ini
+mkdir -p ~/.secrets/certbot && echo '# Cloudflare API token used by Certbot
+dns_cloudflare_api_token = $CF_TOKEN'> ~/.secrets/certbot/cloudflare.ini && chmod 600 ~/.secrets/certbot/cloudflare.ini
