8561427a54c7098d067d39d11497ea351e24ad54
Edson Jiménez <51224220+Edsonytic@users.noreply.github.com>
2023-01-03 03:24:34 -0600

    ensure writing of token to file
    Separated file creation for token into a step for each line and added a
    step to create the string before writing

--- install/certbot.sh
+++ install/certbot.sh
@@ -2,5 +2,6 @@
 
 sudo apt-get install -y certbot python3-certbot-dns-cloudflare python3-certbot-apache
 read -p 'Please provide an API Token for DNS on Cloudflare: ' CF_TOKEN
-mkdir -p ~/.secrets/certbot && echo '# Cloudflare API token used by Certbot
-dns_cloudflare_api_token = $CF_TOKEN'> ~/.secrets/certbot/cloudflare.ini && chmod 600 ~/.secrets/certbot/cloudflare.ini
+tokenstring= "dns_cloudflare_api_token = $CF_TOKEN"
+mkdir -p ~/.secrets/certbot && echo '# Cloudflare API token used by Certbot'> ~/.secrets/certbot/cloudflare.ini && echo "$tokenstring">> ~/.secrets/certbot/cloudflare.ini
+chmod 600 ~/.secrets/certbot/cloudflare.ini
